51423fb7488622f972dc5e3846c35c56e04e349d
Prajal Patidar <prajalpatidar01@gmail.com>
2022-06-28 17:27:07 +0530

    changes in auth,regex

--- utils/auth.js
+++ utils/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/users");
 const getAuthenticated = async function (token) {
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
   const user = await User.findById(decoded._id);
-  if (!customer) {
+  if (!user) {
     throw new Error("Unauthorized");
   } else {
     return user;
@@ -15,7 +15,7 @@ const auth = async function (req, res, next) {
   try {
     const Authorization = req.header("Authorization");
     if (!Authorization) {
-      res.status(401).send({ error: "Auth-key Not found" });
+      return res.status(401).send({ error: "Auth-key Not found" });
     }
     const token = Authorization.replace("Bearer ", "");
     req.token = token;
