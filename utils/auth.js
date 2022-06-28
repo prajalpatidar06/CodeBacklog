7cd206e8a807ded03869b5239c702444d3a5c94f
Prajal Patidar <prajalpatidar01@gmail.com>
2022-06-28 08:53:23 +0530

    add auth,regex to utils

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,8 @@
 const jwt = require("jsonwebtoken");
-const config = require("../config");
 const User = require("../models/users");
 
 const getAuthenticated = async function (token) {
-  const decoded = jwt.verify(token, config.JWT_SECRET);
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
   const user = await User.findById(decoded._id);
   if (!customer) {
     throw new Error("Unauthorized");
@@ -12,7 +11,7 @@ const getAuthenticated = async function (token) {
   }
 };
 
-module.exports = async function (req, res, next) {
+const auth = async function (req, res, next) {
   try {
     const Authorization = req.header("Authorization");
     if (!Authorization) {
@@ -28,4 +27,30 @@ module.exports = async function (req, res, next) {
       .status(401)
       .send({ error: "Please authenticate", message: e.message });
   }
+};
+
+const isAuthenticated = async function (req, res, next) {
+  try {
+    const Authorization = req.header("Authorization");
+    if (!Authorization) {
+      req.user = null;
+      req.isAuthenticated = false;
+      next();
+    }
+    const token = Authorization.replace("Bearer ", "");
+    req.user = await getAuthenticatedUser(token);
+    req.token = token;
+    req.isAuthenticated = true;
+    next();
+    return;
+  } catch (e) {
+    req.user = null;
+    req.isAuthenticated = false;
+    next();
+  }
+};
+
+module.exports = {
+  auth,
+  isAuthenticated,
 };
