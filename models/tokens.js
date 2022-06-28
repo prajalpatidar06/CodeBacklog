feb2a20c3ed224039906ab9e708c712ba0e5ffb4
Prajal Patidar <prajalpatidar01@gmail.com>
2022-06-28 08:54:14 +0530

    add tokens model

---
+++ models/tokens.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const TokenSchema = new mongoose.Schema({
+  id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+    unique: "true",
+  },
+  token: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, expires: 3600 },
+});
+
+const Token = mongoose.model("Token", TokenSchema);
+
+module.exports = Token;
