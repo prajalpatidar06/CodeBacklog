92c926d64b76ac26fec7451c6c5eb3c15bd0a30e
Prajal Patidar <prajalpatidar01@gmail.com>
2022-06-28 08:54:26 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const http = require("http");
 const https = require("https");
 const fs = require("fs");
-const config = require("./config");
 const mongoose = require("mongoose");
-const sendEmail = require("./utils/sendEmail");
+const bodyParser = require("body-parser");
 
+app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    parameterLimit: 100000,
+    extended: true,
+  })
+);
+app.use(cors());
+app.use("/", require("./routers"));
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(
   {
@@ -17,17 +28,17 @@ const httpsServer = https.createServer(
 );
 
 mongoose
-  .connect(config.MONGO_URL, {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to DB");
-    httpServer.listen(config.httpPort, () => {
-      console.log("App listninig in port" + config.httpPort);
+    httpServer.listen(process.env.httpPort, () => {
+      console.log("App listninig in port" + process.env.httpPort);
     });
-    httpsServer.listen(config.httpsPort, () => {
-      console.log("App listning in port" + config.httpsPort);
+    httpsServer.listen(process.env.httpsPort, () => {
+      console.log("App listning in port" + process.env.httpsPort);
     });
   })
   .catch((e) => {
