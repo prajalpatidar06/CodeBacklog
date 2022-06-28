2e89eca7fba0de80bfd4f4e3d00df48d47cda89f
Prajal Patidar <prajalpatidar01@gmail.com>
2022-06-28 17:26:44 +0530

    add problems model

---
+++ models/problems.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const ProblemsSchema = new mongoose.Schema({
+  problemUrl: { type: String, required: true },
+  notes: { type: String },
+  status: { type: Boolean, default: false },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  code: { type: String },
+  language: {
+    type: String,
+    enum: ["none", "c", "cpp", "python", "java", "javascript"],
+    default: "none",
+  },
+});
+
+const Problem = mongoose.model("Problem", ProblemsSchema);
+module.exports = Problem;
