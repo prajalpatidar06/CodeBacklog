5c10b848e251bf2a55cf455838aa2a12bbec4a27
Prajal Patidar <prajalpatidar01@gmail.com>
2022-06-29 15:52:47 +0530

    define initial route route

--- routers/index.js
+++ routers/index.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 router.use("/users", require("./users"));
 router.use("/problems", require("./problems"));
 router.get("/", (req, res) => {
-  res.send("ping");
+  res.sendFile("home.html", { root: __dirname });
 });
 module.exports = router;
