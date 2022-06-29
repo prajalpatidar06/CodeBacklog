4cef789709c4159090ff6c753585e4fad30d9dbc
Prajal Patidar <prajalpatidar01@gmail.com>
2022-06-29 13:56:14 +0530

    define problems routes and controllers

--- controllers/problems_controller.js
+++ controllers/problems_controller.js
@@ -1,6 +1,45 @@
 const { regex, problemObjectValidation } = require("../utils/regex");
 const Problem = require("../models/problems");
 
+module.exports.getAllProblems = async function (req, res) {
+  try {
+    const problems = await Problem.find()
+      .populate({
+        path: "userId",
+        select: {
+          username: 1,
+          name: 1,
+          email: 1,
+        },
+      })
+      .sort({ createdAt: -1 });
+    res.status(200).send({ success: true, problems });
+  } catch (error) {
+    res.status(400).send({ success: false, error: error.message });
+  }
+};
+
+module.exports.getAutherProblems = async function (req, res) {
+  try {
+    let data = await Problem.find({ userId: req.user.id }).sort({
+      createdAt: -1,
+    });
+    res.status(200).send({ success: true, data });
+  } catch (error) {
+    res.status(400).send({ success: false, error: error.message });
+  }
+};
+
+module.exports.getProblemById = async function (req, res) {
+  try {
+    let id = req.params.id;
+    let data = await Problem.findById(id);
+    res.status(200).send({ success: true, data });
+  } catch (error) {
+    res.status(400).send({ success: false, error: error.message });
+  }
+};
+
 module.exports.createProblem = async function (req, res) {
   try {
     let data = problemObjectValidation(req.body);
@@ -19,19 +58,53 @@ module.exports.createProblem = async function (req, res) {
   }
 };
 
-module.exports.getAllProblems = async function (req, res) {
+module.exports.updateProblem = async function (req, res) {
   try {
-    const problems = await Problem.find()
-      .populate({
-        path: "userId",
-        select: {
-          username: 1,
-          name: 1,
-          email: 1,
-        },
-      })
-      .sort({ createdAt: -1 });
-    res.status(200).send({ success: true, problems });
+    let id = req.params.id;
+    let data = await Problem.findById(id);
+    if (data && data.userId == req.user.id) {
+      if (
+        req.body.problemUrl &&
+        typeof req.body.problemUrl == "string" &&
+        regex("url", req.body.problemUrl.trim())
+      ) {
+        data.problemUrl = req.body.problemUrl.trim();
+      }
+      if (
+        req.body["status"] !== undefined &&
+        typeof req.body.status == "boolean"
+      ) {
+        data.status = req.body.status;
+      }
+      if (req.body.notes && typeof req.body.notes == "string") {
+        data.notes = req.body.notes.trim();
+      }
+      if (req.body.code && typeof req.body.code == "string") {
+        data.code = req.body.code.trim();
+      }
+      if (req.body.language && typeof req.body.language == "string") {
+        data.language = req.body.language.trim();
+      }
+      await data.save();
+      res.status(200).send({ success: true, data });
+    } else {
+      res.status(400).send({ success: false, error: "invalid id" });
+    }
+  } catch (error) {
+    res.status(400).send({ success: false, error: error.message });
+  }
+};
+
+module.exports.deleteProblem = async function (req, res) {
+  try {
+    let id = req.params.id;
+    let data = await Problem.findById(id);
+    if (data && data.userId == req.user.id) {
+      await data.delete();
+      res.status(200).send({ success: true });
+    } else {
+      res.status(400).send({ success: false, error: "invalid id" });
+    }
   } catch (error) {
     res.status(400).send({ success: false, error: error.message });
   }
--- routers/problems.js
+++ routers/problems.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 const { auth } = require("../utils/auth");
 const problemsController = require("../controllers/problems_controller");
 
-router.post("/", auth, problemsController.createProblem);
 router.get("/", problemsController.getAllProblems);
+router.get("/me", auth, problemsController.getAutherProblems);
+router.get("/:id", problemsController.getProblemById);
+router.post("/", auth, problemsController.createProblem);
+router.put("/:id", auth, problemsController.updateProblem);
+router.delete("/:id", auth, problemsController.deleteProblem);
 
 module.exports = router;
